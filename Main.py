abcdbb5498c2be98c1954a1154c753114910a1e4
Manjas Anand <manjasanand08@gmail.com>
2026-06-19 09:20:08 +0530

    Add simple calculator functionality in Main.py
    Implemented a simple calculator that performs addition, subtraction, multiplication, and division based on user input.

---
+++ Main.py
@@ -0,0 +1,41 @@
+print("SIMPLE CALCULATOR")
+
+print("Please choose a operator")
+
+print("1.Addition") #Choose it for adding your numbers
+
+print("2.Subtraction")#Choose it for subtracting your numbers
+
+print("3.Multiply")#Choose it for multiplying your numbers
+
+print("4.Division")#Choose it for dividing your numbers
+
+a=int(input("Choose a operator 1, 2, 3 or 4 :")) #Choose the operation you want to perform
+
+b=int(input("Enter greater number:"))
+
+c=int(input("Enter smaller number:"))
+
+if a==1:
+
+print(b,"+",c,"=",b+c)
+
+elif a==2:
+
+print(b,"-",c,"=" ,b-c)
+
+elif a==3:
+
+print(b,"*",c,"=",b*c)
+
+elif a==4:
+
+print(b,"/",c,"=" ,b/c)
+
+else:
+
+print("Invalid input")
+
+#CREATED BY MANJAS ANAND
+
+print(“Thank You For Using!”)
